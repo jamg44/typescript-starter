1f70838318c9e061dd62183c8eb4e59a1779d389
jgonzalezsenti <jgonzalez@sentisis.com>
2025-11-02 13:33:45 +0100

    node 24 & fix logger

--- src/common/utils/logger.ts
+++ src/common/utils/logger.ts
@@ -4,15 +4,21 @@ import { env } from "@/common/utils/envConfig"
 // If you want to customize the logger on your application, you can do:
 // const log = logger.child({ name: 'service-name' });
 
-const logger = pino({
-	level: env.LOG_LEVEL || "info",
+const developmentOptions = {
 	transport: {
 		target: "pino-pretty",
 		options: {
 			colorize: true,
-			translateTime: "SYS:standard", // Use a standard system format: "yyyy-mm-dd HH:MM:ss.l o", UTC alternative: "yyyy-mm-dd HH:MM:ss.l Z"
+			// Use a standard system format: "yyyy-mm-dd HH:MM:ss.l o", UTC alternative: "yyyy-mm-dd HH:MM:ss.l Z"
+			// translateTime: "SYS:standard",
+			ignore: "pid,hostname",
 		},
 	},
+}
+
+const logger = pino({
+	level: env.LOG_LEVEL || "info",
+	...(env.isDevelopment && developmentOptions),
 })
 
 export default logger
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,28 @@
 import { env } from "@/common/utils/envConfig"
 import logger from "@/common/utils/logger.js"
 
+process.on("SIGINT", onCloseSignal)
+process.on("SIGTERM", onCloseSignal)
+process.on("exit", code => logger.info(`Exiting with code: ${code}`))
 logger.info({ env: env.NODE_ENV, log_level: env.LOG_LEVEL }, "Service starting...")
 
-const onCloseSignal = () => {
+async function main() {
+	logger.info("Hello, World!")
+}
+
+main().catch(err => { console.error("Fatal error", err); process.exit(1) })
+
+/**
+ * Functions
+ * **********************************
+ */
+
+function onCloseSignal() {
 	logger.info("sigint received, shutting down")
 	// connection.close(() => {
 	// 	logger.info("conn closed");
 	// 	process.exit();
 	// });
 	setTimeout(() => process.exit(1), 10000).unref() // Force shutdown after 10s
+	process.exit()
 }
-
-process.on("SIGINT", onCloseSignal)
-process.on("SIGTERM", onCloseSignal)
